00d0da72cbe25fde386a606aff31e7bec25f821b
maxim880000 <mdholberton@gmail.com>
2025-10-21 09:33:26 +0200

    hello4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints "Programming is like building a multilingual puzzle"
+ * to the standard output using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+ char main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
