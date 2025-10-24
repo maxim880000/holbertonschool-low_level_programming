3cb3c6d4d97461d65155fa7fd5d10c9c071c9f89
maxim880000 <mdholberton@gmail.com>
2025-10-24 17:24:15 +0200

    loops7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10;
-	if (last < 0)
-		last = -last;
+	last = n % 10;      /* Récupère le dernier chiffre */
+	if (last < 0)        /* Si le chiffre est négatif */
+		last = -last;    /* le rend positif */
 
-	_putchar(last + '0');
-	return (last);
+	_putchar(last + '0'); /* Affiche le chiffre */
+	return (last);        /* Retourne le chiffre */
 }
