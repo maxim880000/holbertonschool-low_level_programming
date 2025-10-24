c2c0cf546f26149625027ddb36933ce2fe628ecf
maxim880000 <mdholberton@gmail.com>
2025-10-24 14:48:31 +0200

    loops4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * isalpha check all th alphabetic caractere
+ * @c: c to check
+ *
+ * return 1 if c is a letter low or upp if not 0
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
