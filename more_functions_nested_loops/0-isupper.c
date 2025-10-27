a1ad67d01fdfc6563abb21b844d6d5f3dc93b1db
maxim880000 <mdholberton@gmail.com>
2025-10-27 13:44:54 +0100

    nested loops 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase character.
-* @c: The character to check
-*
-* retrun 0 (success _isupper)
-*/
+ * _isupper - checks for uppercase character
+ * @c: The character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 
 int _isupper(int c)
 {
