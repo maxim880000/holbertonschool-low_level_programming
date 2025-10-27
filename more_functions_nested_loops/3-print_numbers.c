f6dcb09e2474d78bcb4bb21d9ad20a94ccbf9ea0
maxim880000 <mdholberton@gmail.com>
2025-10-27 16:34:09 +0100

    nested loops 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,19 +3,19 @@
 /**
  * print_numbers -  prints the numbers, from 0 to 9, followed by a new line.
  * @i: numbers, from 0 to 9, followed by a new line.
- * 
- * retrun: 
+ *
+ * retrun:
  */
 
- void print_numbers(void)
- {
-    int i = 0
-    
-    while (i < 9)
-    {
-        _putchar("%d", i);
-        i = i + 1;
-    }
+void print_numbers(void)
+{
+	int i = 0
 
-    return 0;
- }
+	while (i < 9)
+	{
+		_putchar("%d", i);
+		i = i + 1;
+	}
+
+	return (0);
+}
