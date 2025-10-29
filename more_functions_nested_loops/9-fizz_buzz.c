045f97b5af0f00e08c5e7902a6bef6ecbb25185e
maxim880000 <mdholberton@gmail.com>
2025-10-29 15:08:35 +0100

    nested loops9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,12 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz");
 
-		if else(i % 3 == 0)
-		printf("Fizz")
+		else if (i % 3 == 0)
+		printf("Fizz");
 
 		else
 		printf("Buzz");
 	}
-	putchar('/n');
+	putchar('\n');
 	return (O);
 }
