dcfeea93acd34ce03408b56c72737b7834250cd7
maxim880000 <mdholberton@gmail.com>
2025-10-29 15:00:47 +0100

    nested loops9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints the numbers from 1 to 100
+*
+* Return : == 0 (succefuly)
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+
+		if else(i % 3 == 0)
+		printf("Fizz")
+
+		else
+	}
+	putchar('/n');
+	return (O);
+}
