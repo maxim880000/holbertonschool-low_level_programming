522c4f92ba72f0ad026ed623e88957721ebea651
maxim880000 <mdholberton@gmail.com>
2025-10-30 14:40:28 +0100

    nested loops8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
 /**
- * print_square - prints a square using the character #, followed by a new line
- * @size: size of the square
+ * print_square - Imprime un carré de dièses.
+ * @size: La taille (côté) du carré.
  *
- * Return: void
+ * Description: Utilise des boucles imbriquées pour imprimer un carré
+ * de 'size' lignes et 'size' colonnes. Si 'size' est 0 ou négatif,
+ * imprime seulement un retour à la ligne.
  */
 void print_square(int size)
 {
-	int i, j;
+	int ligne;
+	int col;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		for (j = 0; j < size; j++)
-			_putchar('#');
-
-		_putchar('\n');
+		for (ligne = 0; ligne < size; ligne++)
+		{
+			for (col = 0; col < size; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
