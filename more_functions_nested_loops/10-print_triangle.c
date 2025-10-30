94047114a4770bb84045aabf8fb5bdd153347383
maxim880000 <mdholberton@gmail.com>
2025-10-30 20:42:04 +0100

    nested loops10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,5 +8,22 @@
  */
 void print_triangle(int size)
 {
-	
+	int x, y, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (x = 1; x <= size; x++)
+	{
+		for (space = size - x; space > 0; space--)
+			_putchar(' ');
+
+		for (y = 0; y < x; y++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
 }
