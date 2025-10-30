a12425b71f44259dcd19b41471b7809dbfda2f07
maxim880000 <mdholberton@gmail.com>
2025-10-30 18:21:41 +0100

    nested loops8

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,5 @@
  */
 void print_triangle(int size)
 {
-	int i, j, k;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-
-	for (i = 1; i <= size; i++)
-	{
-		/* Print spaces */
-		for (j = size - i; j > 0; j--)
-			_putchar(' ');
-
-		/* Print # */
-		for (k = 0; k < i; k++)
-			_putchar('#');
-
-		_putchar('\n');
-	}
+	
 }
--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,37 +4,21 @@
  * print_square - Imprime un carré de dièses, suivi d'une nouvelle ligne.
  * @size: La taille (côté) du carré.
  *
- * Description: La fonction utilise des boucles imbriquées pour imprimer
- * un carré de 'size' lignes et 'size' colonnes.
- * Si 'size' est 0 ou négatif, la fonction imprime seulement
- * un retour à la ligne.
  */
 void print_square(int size)
 {
-	int ligne; /* Compteur pour les lignes (vertical) */
-	int col;   /* Compteur pour les colonnes (horizontal) */
+	int x;
+	int y;
 
-	/* 1. Gérer le cas spécial où size est 0 ou négatif */
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
-	else
-	{
-		/* 2. Boucle Extérieure : s'occupe des lignes */
-		for (ligne = 0; ligne < size; ligne++)
-		{
-			/* 3. Boucle Intérieure : s'occupe des colonnes */
-			/* Pour chaque ligne, on imprime 'size' dièses */
-			for (col = 0; col < size; col++)
-			{
-				_putchar('#');
-			}
 
-			/* 4. Fin de la ligne */
-			/* Une fois tous les dièses de la ligne imprimés, */
-			/* on passe à la ligne suivante. */
-			_putchar('\n');
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
 		}
+			_putchar('\n');
 	}
 }
