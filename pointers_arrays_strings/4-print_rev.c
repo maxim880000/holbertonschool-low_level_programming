b3fe8e802aa3233b655e75f27473fee4dc1386e2
maxim880000 <mdholberton@gmail.com>
2025-10-31 12:18:31 +0100

    ptr4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ */
+void print_rev(char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+        i++;
+
+    for (i--; i >= 0; i--)
+        _putchar(s[i]);
+
+    _putchar('\n');
+}
