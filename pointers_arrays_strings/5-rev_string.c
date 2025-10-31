3468e966c64c97ad188b1eeb2f170c31e716a919
maxim880000 <mdholberton@gmail.com>
2025-10-31 14:52:34 +0100

    ptr5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - Inverse une chaîne de caractères "sur place" (in-place).
+ * @s: Le pointeur vers la chaîne à inverser.
+ *
+ * Description: Utilise la méthode des "deux pointeurs". 
+ */
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char temp;
+
+	while (s[end] != '\0')
+	{
+		end++;
+	}
+	end--;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
+
