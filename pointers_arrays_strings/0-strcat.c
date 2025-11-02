73b2e2ea5f6dd109564f7a6a905f991fcdd55181
maxim880000 <mdholberton@gmail.com>
2025-11-02 15:26:47 +0100

    ptr2.0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strcat - fonction pour coller deux partie 
+* 
+* Return : return(dest)
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int l_dest = 0;
+
+	while (dest[l_dest] != '\0')
+	{
+		l_dest++;
+	}
+
+	int l_src =0;
+
+	while (src[l_src] != '\0')
+	{
+		dest[l_dest] = src[i_src];
+		l_dest++;
+		l_src++;
+	}
+
+	dest[len_dest] = '\0';
+
+	return (dest);
+}
