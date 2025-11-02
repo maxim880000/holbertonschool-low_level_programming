748e8084c5ae9eba5c72e295d4660038f3f4fb59
maxim880000 <mdholberton@gmail.com>
2025-11-02 16:07:10 +0100

    ptr2.5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncpy - copies a string
+* @dest: destination string
+* @src: source string
+* @n: maximum number of bytes to copy
+*
+* Return: pointer to dest
+*/
+
+char *string_toupper(char *)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	for (; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+
+	return (dest);
+}
