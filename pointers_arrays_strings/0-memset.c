6e1262d0cbab93ad2c515c147e3b13a23b0d2a60
maxim880000 <mdholberton@gmail.com>
2025-11-03 09:49:56 +0100

    ptr3.0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _memset - fills memory with a constant byte.
+* @s: char caractere.
+* @b: char2.
+* @n: unsigned int n.
+* Return: pointer to the memory s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for(i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
