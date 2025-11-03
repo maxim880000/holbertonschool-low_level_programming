b2694a12834765471551b9b92de8d619908282fe
maxim880000 <mdholberton@gmail.com>
2025-11-03 16:17:47 +0100

    ptr3.3 && git push git add . && git commit -m ptr3.

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _strspn - gets the length of a prefix substring.
+* @s: pointer
+* @accept: pointer
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int t = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			t = 0;
+			if (s[i] == accept[j])
+			{
+				t = 1;
+				break;
+			}
+		}
+
+		if (t == 0)
+			return (i);
+	}
+
+	return (i);
+}
