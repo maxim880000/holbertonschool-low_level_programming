d8eeed977be9d1026c1554058746a6262b2dbd68
maxim880000 <mdholberton@gmail.com>
2025-11-04 09:44:34 +0100

    ptr3.7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i, b = 0, c = 0;
+	int i, b = 0, c = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        b += *(a + i * size + i);
-        c += *(a + i * size + (size - 1 - i));
+	for (i = 0; i < size; i++)
+	{
+		b += *(a + i * size + i);
+		c += *(a + i * size + (size - 1 - i));
 	}
-    printf("%d, %d\n", b, c);
+	printf("%d, %d\n", b, c);
 }
