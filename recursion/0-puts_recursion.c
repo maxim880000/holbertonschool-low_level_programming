84c9abd45a999cfcf97f49d98da84a48b61ff729
maxim880000 <mdholberton@gmail.com>
2025-11-04 13:48:13 +0100

    recursion0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* is_prime_number - prints a string, followed by a new line.
+* @s: pointer
+*
+* Return:
+*/
+
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('/n');
+		return;
+	}
+
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
