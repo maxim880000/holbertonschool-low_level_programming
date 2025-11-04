b296dab85574fb501ddacfde357024efa00ffe3c
maxim880000 <mdholberton@gmail.com>
2025-11-04 09:45:23 +0100

    ptr3.5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,22 +11,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (needle[0] == '\0')
-        return (haystack);
+	if (needle[0] == '\0')
+		return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
 
-        if (needle[j] == '\0')
-            return (&haystack[i]);
-    }
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
 
-    return (NULL);
+	return (NULL);
 }
