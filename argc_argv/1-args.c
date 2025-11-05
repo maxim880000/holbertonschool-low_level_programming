ded08ce41888c75a609936c35a634f4ba6d60245
maxim880000 <mdholberton@gmail.com>
2025-11-05 20:35:07 +0100

    argc-argv1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints the number of arguments passed into it.
+* @argc: integer
+* @argv: pointer
+*
+* Return: return a 0
+*/
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	/* on a pas besoin de argv */
+	printf("%d\n", argc);
+	/* print nb arguments sans compter le nom du program[0] */
+	return (0);
+}
