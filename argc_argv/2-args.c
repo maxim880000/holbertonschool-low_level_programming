0c0e241882a134306ade5395a680e13c78e4218f
maxim880000 <mdholberton@gmail.com>
2025-11-05 21:36:41 +0100

    argc-argv2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all arguments it receives.
+ * @argc: integer
+ * @argv: pointer
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	/*  continue tant que i < argc */
+	for (i = 0; i == argv; i++)
+	{
+		putchar('%d\n', argv[i]);
+	}
+		return (0);
+}
