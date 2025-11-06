8867244e46568e43a8c8b354f20c6a16173bd5d4
maxim880000 <mdholberton@gmail.com>
2025-11-06 13:18:05 +0100

    argc-argv3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - multiplies two numbers
+ * main - multiplies 2 numbers
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 if success, 1 if error
  */
 
-int main(int argc; char **argv)
+int main(int argc, char **argv)
 {
 	int a, b, result;
 	/* verrifie si y a 2 arg */
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Erreur\n");
 		return (1);
 	}
 	/* ASCII to Integer convert */
-	a = atoi(argv[2]);
-	b = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
 	result = a * b;
 	printf("%d\n", result);
