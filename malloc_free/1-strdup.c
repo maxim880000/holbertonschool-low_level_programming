f56585ecd5b5ffc4d507ce308a4f7bf0f26478f9
maxim880000 <mdholberton@gmail.com>
2025-11-10 14:11:53 +0100

    malloc1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/*
+* _strdup - returns a pointer to a newly allocated space in memory
+* @str: pointer chaine
+*
+* Return:
+*/
+char *_strdup(char *str)
+{
+	int leng;
+	char *sstr;
+	int i;
+
+	/* conditions si la chaine est null pour pas avoir d'erreur */
+	if (str == 0)
+		return (NULL);
+
+	/* calculer la longueur */
+	for (leng = 0; str[leng] != '\0'; leng++);
+
+	/* alloue de la memoire +1 car malloc prend pas \0 */
+	sstr = malloc((leng + 1) * sizeof(char));
+	if (sstr == NULL)
+		return (NULL);
+
+	/* transferer les chaine de char avec i*/
+	for (i = 0; i < leng; i++)
+		sstr[i] = str[i];
+
+	sstr[leng] = '\0';
+
+	return (sstr);
+}
