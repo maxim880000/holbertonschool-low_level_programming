7c993caa79400c19355953efce76a2462fc499dc
maxim880000 <mdholberton@gmail.com>
2025-11-10 16:35:23 +0100

    malloc2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,17 +17,20 @@ int i;
 
 /* si on recoit un pointeur vide on le met en "" */
 if (s1 == NULL)
+{
 	s1 = "";
+}
 if (s2 == NULL)
+{
 	s2 = "";
-
+}
 	/* boucle vide pour compter les char */
 	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
 	;
 	
 	for (leng2 = 0; s2[leng2] != '\0'; leng2++)
 	;
-	result = malloc((leng1 + leng2 + 1) * sizeof(char))
+	result = malloc((leng1 + leng2 + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 
