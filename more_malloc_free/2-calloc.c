ff4cc9508dcd2eb6b14b5b00a1bbdc62182a4274
maxim880000 <mdholberton@gmail.com>
2025-11-12 13:38:46 +0100

    malloc2.2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= result; i++)
+	for (i = 0; i < result; i++)
 		ptr[i] = 0;
 		return ((void *) ptr);
 
