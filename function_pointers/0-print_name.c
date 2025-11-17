10604d5d033a7bb49ae1af37acd41372eb53fc6d
maxim880000 <mdholberton@gmail.com>
2025-11-17 14:26:12 +0100

    functionspointer0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -13,5 +13,7 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+	/* f: variable qui contient l'adresse d'une fonction */
+	/* f(name) → va à cette adresse, exécute la fonction et lui passe name */
 	f(name);
 }
