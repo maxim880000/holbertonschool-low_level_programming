7bc0dc2793550c071001a23eb332848799a0b1a2
maxim880000 <mdholberton@gmail.com>
2025-11-17 15:27:28 +0100

    functionspointer2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: pointer vers un tableau
+ * @size: nb element
+ * @cmp: pointer to compares values
+ *
+ * Return: -1 if not found else r(i)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i])
+			if (cmp(array[i]) != 0)
+				return (i);
+	}
+		return (-1);
+}
