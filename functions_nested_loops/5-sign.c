385896f807716081ede98d28a5b61c8370f39f7f
maxim880000 <mdholberton@gmail.com>
2025-11-17 09:07:59 +0100

    functions nested loop5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,19 +8,22 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+/* Check if number is positive */
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+/* Check if number is zero */
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+/* Otherwise, number is negative */
+else
+{
+_putchar('-');
+return (-1);
+}
 }
