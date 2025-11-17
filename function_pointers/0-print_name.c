ba253c9475acf995eef2cf51c86dfbe0e9373fd6
maxim880000 <mdholberton@gmail.com>
2025-11-17 14:03:44 +0100

    functionspointer0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name of the person
- * @f: pointer to function which will print the name
- */
+* print_name - prints a name
+* @name: name of the person
+* @f: pointer to function which will print the name
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
