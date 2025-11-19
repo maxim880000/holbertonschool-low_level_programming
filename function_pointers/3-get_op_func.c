fdbbfe55d1489a31dfda1e71464bd0cc25689143
maxim880000 <mdholberton@gmail.com>
2025-11-19 10:03:50 +0100

    3pointerfocntion

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <string.h>
+/**
+* 
+* 
+* 
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] =
+	{
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: premier nombre
+ * @b: deuxième nombre
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - soustraction
+ * @a: premier
+ * @b: deuxième
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: premier
+ * @b: deuxième
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: dividende
+ * @b: diviseur
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: nombre
+ * @b: diviseur
+ * Return: reste de la division
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
