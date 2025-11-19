a07d7f29b60e97f212ee911501fa15952c6288c5
maxim880000 <mdholberton@gmail.com>
2025-11-19 15:09:40 +0100

    Variadic functions0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,15 +3,15 @@
 /**
 * sum_them_all - returns the sum of all its parameters.
 * @n: integer constant et +
-* 
-* Return: 0 if n == 0 else sum 
+*
+* Return: 0 if n == 0 else sum
 */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i; /* compteur pour la boucle */
 	va_list boite; /* "curseur" pour argmt variadique */
-	int sum = 0; /* on initialise la somme à 0 */ 
+	int sum = 0; /* on initialise la somme à 0 */
 
 	if (n == 0) /* verifie la première conditionn*/
 		return (0);
@@ -24,5 +24,5 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(boite); /* marque la fin de l'use de boite */
 
-	return (sum); 
+	return (sum);
 }
