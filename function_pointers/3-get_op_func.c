400084fc4bf4548a250dbed8d7f72d5eaf8c0951
maxim880000 <mdholberton@gmail.com>
2025-11-21 14:01:28 +0100

    functionspointer3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,29 +1,36 @@
 #include "3-calc.h"
 #include <string.h>
-/**
-* 
-* 
-* 
-*/
 
+/**
+ * get_op_func - selects the right function to perform the operation
+ * @s: the operator passed as argument ("+", "-", "*", "/", "%")
+ *
+ * Return: pointer to the function that matches the operator
+ * ou NULL 
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
 
-	op_t ops[] =
-	{
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+	/* tableau des opérateurs et des fonctions associées */
+	op_t ops[] = {
+		{"+", op_add},   /* addition */
+		{"-", op_sub},   /* soustraction */
+		{"*", op_mul},   /* multiplication */
+		{"/", op_div},   /* division */
+		{"%", op_mod},   /* modulo */
+		{NULL, NULL}     /* fin du tableau */
 	};
 
-	for (i = 0; ops[i].op != NULL; i++)
+	/* on parcourt le tableau jusqu'à la fin */
+	while (ops[i].op != NULL)
 	{
-		if ()
-			return (ops[i].f);
+		/* si l'opérateur correspond exactement (strcmp retourne 0) */
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);   /* on retourne le pointeur vers la fonction */
+		i++;
 	}
+
+	/* si on n'a rien trouvé → opérateur inconnu */
 	return (NULL);
 }
