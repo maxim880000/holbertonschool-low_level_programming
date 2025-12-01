dc98f21e0b99c80cda744a5a0a8005842e687fa7
maxim880000 <mdholberton@gmail.com>
2025-12-01 16:03:38 +0100

    task1 finish

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: node of the list
+ *
+ * Return: number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	/* tant qu’on a un nœud valide */
+	while (ptr != NULL)
+	{
+		count++; /* count the current node */
+		ptr = ptr->next; /* advance to the next node */
+	}
+}
