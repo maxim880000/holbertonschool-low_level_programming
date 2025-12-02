704ec16ee46158291e4bd1bba00e58b24288db64
maxim880000 <mdholberton@gmail.com>
2025-12-02 10:54:04 +0100

    committed last part of 2_add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: doubl epointeur qui pointe vers le haut de la liste
+ * @str: string to add
+ *
+ * Return: le nouveau noeud or null if it's fail
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr; /* nouveau noeud */
+
+	/* allouer de la memoire pour le new noeud */
+	/* si sa echoue on sort */
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	/* je fais une copie du string pour mon nœud */
+	ptr->str = strdup(str);
+
+	/* count the nb of letter of the string and put it in ptr->len */
+	ptr->len = strlen(str);
+
+	/* le nouveau noeud pointe-> l'ancien 1ER noeud */
+	ptr->next = *head;
+	*head = ptr; /* nouveau nœud devient le premier de la list*/
+
+	return (ptr); /* je retourne le nouveau nœud */
+}
