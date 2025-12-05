83c01d86508978778be9ceb127b0c924675b4b53
maxim880000 <mdholberton@gmail.com>
2025-12-05 19:40:16 +0100

    committed task 3

--- file_io/main.h
+++ file_io/main.h
@@ -11,6 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int code, const char *msg, const char *arg);
+int main(int argc, char *argv[]);
 int _putchar(char c);
 
 #endif
