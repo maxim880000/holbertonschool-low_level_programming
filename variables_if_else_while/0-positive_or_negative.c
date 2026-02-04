3046a4fc4119c44c73d461130d31834e583b31b5
maxim880000 <mdholberton@gmail.com>
2026-02-04 16:43:37 +0100

    committed a review of if else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,25 +7,27 @@
 *
 * Return: toujours 0 (succès)
 */
-
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	/* srand(time(0)) : initialise le générateur de nombres aléatoires */
+	srand(time(0));
 
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-return (0);
+	/* rand() génère un nombre aléatoire entre 0 et RAND_MAX
+	RAND_MAX est la valeur maximale possible renvoyée par rand()
+	RAND_MAX/2 : on décale le nombre pour qu'il puisse être positif ou négatif,
+	centré autour de 0 */
+	n = rand() - RAND_MAX / 2;
+
+	/* Vérifie si le nombre est positif, nul ou négatif */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	/* Retourne 0 pour indiquer succès */
+	return (0);
 }
--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,22 +2,33 @@
 #include <time.h>
 #include <stdio.h>
 
+/* génère un nombre aléatoire, récupère son dernier chiffre
+et indique si ce chiffre est >5, =0, ou <6 et non nul */
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n, last_digit;
 
+	/* Initialise le générateur de nombres aléatoires
+	pour que rand() donne des résultats différents à chaque exécution */
 	srand(time(0));
+
+	/* rand() génère un nombre aléatoire entre 0 et RAND_MAX
+	On soustrait RAND_MAX/2 pour centrer le nombre autour de 0 */
 	n = rand() - RAND_MAX / 2;
 
+	/* Récupère le dernier chiffre du nombre */
 	last_digit = n % 10;
 
+	/* Affiche le nombre et son dernier chiffre */
 	printf("Last digit of %d is %d ", n, last_digit);
 
+	/* Vérifie si le dernier chiffre est >5, =0, ou autre */
 	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
@@ -31,5 +42,6 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	}
 
+	/* Retourne 0 pour indiquer succès */
 	return (0);
 }
